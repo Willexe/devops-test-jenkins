a5b027cc4f88608e747a46b7238cdf6c046faff9
Willexe <isagiyoichi427@gmail.com>
2025-08-12 21:57:46 -0300

    Colocando o node no jenkins

--- api/index.js
+++ api/index.js
@@ -1,17 +1,17 @@
-import userRoutes from "./routes/users.js"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/users.js";
 
-const express = require("express");
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+app.use("/", userRoutes);
 
-app.use("/", userRoutes)
-
-app.listen(3001, '0.0.0.0', () => {
-  console.log("Servidor backend rodando na porta 3001")
-
+app.listen(3001, "0.0.0.0", () => {
+  console.log("Servidor backend rodando na porta 3001");
 });
 
-module.exports = app;
+export default app;
+
+
